933e25746559513f130656e6162bad9b0c062af2
Victor-HM <victor.27cm.santos@gmail.com>
2022-08-05 16:41:28 -0300

    nome salvo

--- App.js
+++ App.js
@@ -1,23 +1,52 @@
-import React,{useState} from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput} from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity, 
+  TextInput,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [input, setInput] = useState("");
 
-const [name, setName] = useState('Nicolle chata')
-const [input,setInput] = useState('')
+  useEffect(() => {
+    async function mostrarNome() {
+      const nomeSalvo = await AsyncStorage.getItem('nome');
+      
+      if(nomeSalvo !== null) {
+        setName(nomeSalvo)
+      }
+    }
 
-function trocaNome() {
+    mostrarNome()
+  }, [])
 
-  setName(input)
+  useEffect(() => {
+    async function salvarName() {
+      await AsyncStorage.setItem('nome', name)
+    }
 
-}
+    salvarName()
+  }, [name])
+
+  function trocaNome() {
+    setName(input);
+  }
 
   return (
     <View style={styles.container}>
-    <TextInput style = {styles.input} value = {input} onChangeText = {(texto) => setInput(texto)} />
-    <TouchableOpacity style = {styles.botao} onPress = {()=> trocaNome()} > TROCAR </TouchableOpacity>
+      <TextInput
+        style={styles.input}
+        value={input}
+        onChangeText={(texto) => setInput(texto)}
+      />
+      <TouchableOpacity style={styles.botao} onPress={() => trocaNome()}>
+        TROCAR
+      </TouchableOpacity>
       <Text> {name} </Text>
-
     </View>
   );
 }
@@ -25,23 +54,24 @@ function trocaNome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+
   botao: {
-    width:'90%',
-    backgroundColor: 'orange',
-    alignItems: 'center',
-    margimTop: '10',
+    width: "90%",
+    backgroundColor: "orange",
+    alignItems: "center",
+    margimTop: "10",
   },
+
   input: {
     height: 35,
-    width: '90%',
+    width: "90%",
     marginTop: 10,
     borderRadius: 3,
     borderWidth: 2,
     margin: 5,
-   
-  }
+  },
 });
